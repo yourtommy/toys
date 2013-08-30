dd6edc35d27f5f9d42246740d2be59d525740fc7
Zheng Peng <pezheng@paypal.com>
2013-08-30 10:14:56 +0800

    Reduce main file code.

---
+++ OpenGLToy/src/HelloWorld.h
@@ -0,0 +1,19 @@
+/*
+ * HelloWorld.h
+ *
+ *  Created on: Aug 30, 2013
+ *      Author: pezheng
+ */
+
+#ifndef HELLOWORLD_H_
+#define HELLOWORLD_H_
+
+class HelloWorld {
+public:
+	HelloWorld();
+	~HelloWorld();
+
+	static int main(int argc, char** argv);
+};
+
+#endif /* HELLOWORLD_H_ */
